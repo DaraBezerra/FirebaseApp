a3f3ed22a7f73adc24c3c8573b98a54a723e79ae
dara.lima211 <dara.lima211@gmail.com>
2026-03-17 23:43:02 +0000

    Semana 4

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart'; 
 
+import 'package:firebase_auth/firebase_auth.dart'; 
+
 import 'firebase_options.dart'; 
+
+import 'login.dart'; 
 
 import 'welcome.dart'; 
 
@@ -36,13 +40,53 @@ class MyApp extends StatelessWidget {
 
      theme: ThemeData(primarySwatch: Colors.indigo), 
 
-     home: const HomePage(), 
+     home: const AuthGate(), 
 
    ); 
 
  } 
 
 } 
+
+ 
+
+class AuthGate extends StatelessWidget { 
+
+ const AuthGate({super.key}); 
+
+ @override 
+
+ Widget build(BuildContext context) { 
+
+   return StreamBuilder<User?>( 
+
+     stream: FirebaseAuth.instance.authStateChanges(), 
+
+     builder: (_, snap) { 
+
+       if (snap.connectionState == ConnectionState.waiting) { 
+
+         return const Scaffold( 
+
+           body: Center(child: CircularProgressIndicator()), 
+
+         ); 
+
+       } 
+
+       final user = snap.data; 
+
+       return user == null ? const LoginPage() : const HomePage(); 
+
+     }, 
+
+   ); 
+
+ } 
+
+} 
+
+ 
 
 class HomePage extends StatelessWidget { 
 
@@ -57,6 +101,34 @@ class HomePage extends StatelessWidget {
    return Scaffold( 
 
      appBar: AppBar(title: const Text("Home")), 
+
+     drawer: Drawer( 
+
+       child: ListView( 
+
+         padding: EdgeInsets.zero, 
+
+         children: [ 
+
+           ListTile( 
+
+             leading: const Icon(Icons.logout), 
+
+             title: const Text('Sair'), 
+
+             onTap: () async { 
+
+               await FirebaseAuth.instance.signOut(); 
+
+             }, 
+
+           ), 
+
+         ], 
+
+       ), 
+
+     ), 
 
      body: const Center(child: WelcomeMessage()), 
 
